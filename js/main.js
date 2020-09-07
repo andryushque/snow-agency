40400db44c04c65ed8a00a99e5d7c0142881a71c
andryushque <andrew.ustinov23@gmail.com>
2020-09-07 12:59:56 +0300

    Fix: mmenu button js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
   /*=== Mobile Menu ===*/
   // Show and hide navigation menu
-  var mmenuButton = $("..mmenu__icon");
+  var mmenuButton = $(".mmenu");
+  var mmenuButtonIcon = $(".mmenu__icon");
   var navigationMenu = $(".nav");
   mmenuButton.on("click", function () {
-    mmenuButton.toggleClass("fa-times"); //hamburger button
-    mmenuButton.toggleClass("fa-bars"); //close button
+    mmenuButtonIcon.toggleClass("fa-times"); //hamburger button
+    mmenuButtonIcon.toggleClass("fa-bars"); //close button
     navigationMenu.toggleClass("nav--hidden");
   });
 });
